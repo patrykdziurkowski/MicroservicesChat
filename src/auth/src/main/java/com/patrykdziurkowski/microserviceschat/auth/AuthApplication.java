ea816d9e8e23f4d0f61cde7e6f6871d830806e3b
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2024-10-13 13:26:39 +0200

    Scaffolded the project structure

---
+++ src/auth/src/main/java/com/patrykdziurkowski/microserviceschat/auth/AuthApplication.java
@@ -0,0 +1,13 @@
+package com.patrykdziurkowski.microserviceschat.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
