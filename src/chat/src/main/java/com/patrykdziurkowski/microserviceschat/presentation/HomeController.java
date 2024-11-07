9e4bdf3f7749f510b7483ca97f5d2e807f44ed64
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2024-11-07 13:17:29 +0100

    Scaffolded the remaining webpage endpoints

--- src/chat/src/main/java/com/patrykdziurkowski/microserviceschat/presentation/HomeController.java
+++ src/chat/src/main/java/com/patrykdziurkowski/microserviceschat/presentation/HomeController.java
@@ -3,14 +3,38 @@ package com.patrykdziurkowski.microserviceschat.presentation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    public String index(Model model) {
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("title", "Test title changed");
+        model.addAttribute("message", "Test message");
+        return "register";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("title", "Test title changed");
+        model.addAttribute("message", "Test message");
+        return "login";
+    }
+
+    @GetMapping("/chats")
+    public String chats(Model model) {
         model.addAttribute("title", "Test title changed");
         model.addAttribute("message", "Test message");
         return "chats";
+    }
+
+    @GetMapping("/chat/{chatId}")
+    public String chat(
+            @PathVariable String chatId,
+            Model model) {
+        model.addAttribute("title", "Chat " + chatId);
+        model.addAttribute("message", "Test message");
+        return "chat";
     }
 
 }
