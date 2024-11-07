3cebe400ded945a3b917c97f7ba2e1942f44a18b
Szymon Kruk <szymon.kruk01@gmail.com>
2024-11-07 15:13:02 +0100

    Added service class for removing messages

---
+++ src/chat/src/main/java/com/patrykdziurkowski/microserviceschat/application/MessageRemoveCommand.java
@@ -0,0 +1,34 @@
+package com.patrykdziurkowski.microserviceschat.application;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.patrykdziurkowski.microserviceschat.domain.ChatRoom;
+import com.patrykdziurkowski.microserviceschat.domain.UserMessage;
+
+@Service
+public class MessageRemoveCommand {
+    private final MessageRepository messageRepository;
+    private final ChatRepository chatRepository;
+
+    public MessageRemoveCommand(MessageRepository messageRepository, ChatRepository chatRepository) {
+        this.messageRepository = messageRepository;
+        this.chatRepository = chatRepository;
+    }
+
+    public boolean execute(UUID currentUserId, UUID messageId) {
+        Optional<UserMessage> retrievedMessage = messageRepository.getById(messageId);
+        if(retrievedMessage.isEmpty()) {
+            return false;
+        }
+        UserMessage message = retrievedMessage.get();
+        ChatRoom chat = chatRepository.getById(message.getChatRoomId()).get();
+        if(message.delete(currentUserId, chat.getOwnerId()) == false) {
+            return false;
+        }
+        messageRepository.save(message);
+        return true;
+    }
+}
