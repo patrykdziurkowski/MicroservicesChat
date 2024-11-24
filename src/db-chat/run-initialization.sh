8513f31fc885eb613c5babf32b81138cedf4fe3e
Szymon Kruk <szymon.kruk01@gmail.com>
2024-11-24 14:27:11 +0100

    Added loop istead of static timer in run-initialization.sh

--- src/db-chat/run-initialization.sh
+++ src/db-chat/run-initialization.sh
@@ -1,3 +1,6 @@
 # Wait to be sure that SQL Server came up
-sleep 15s | tr -d '\r'
+while ! /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $1 -Q "SELECT 1" -b -No; do
+    echo "Waiting for chatdb database startup..."
+    sleep 5
+done
 /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $1 -Q "CREATE DATABASE chatdb" -No
