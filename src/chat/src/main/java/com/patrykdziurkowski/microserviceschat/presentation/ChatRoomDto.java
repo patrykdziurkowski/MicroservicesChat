f8841cd0696d6ea88ca3a10c665d46e59c33d30e
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2024-11-30 15:10:04 +0100

    The password in joinChatModal is now shown depending on if the chat is password protected.

--- src/chat/src/main/java/com/patrykdziurkowski/microserviceschat/presentation/ChatRoomDto.java
+++ src/chat/src/main/java/com/patrykdziurkowski/microserviceschat/presentation/ChatRoomDto.java
@@ -1,20 +1,22 @@
 package com.patrykdziurkowski.microserviceschat.presentation;
 
-import com.patrykdziurkowski.microserviceschat.domain.ChatRoom;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
+import com.patrykdziurkowski.microserviceschat.domain.ChatRoom;
 
 public class ChatRoomDto {
     private UUID id;
     private String name;
     private boolean isPublic;
     private boolean isMember;
-    private List<UUID> memberIds = new ArrayList<UUID>();
+    private boolean isPasswordProtected;
+    private List<UUID> memberIds = new ArrayList<>();
     private int memberCount;
-    
-    private ChatRoomDto() {}
+
+    private ChatRoomDto() {
+    }
 
     public static ChatRoomDto from(ChatRoom chatRoom, UUID userId) {
         ChatRoomDto chatDto = new ChatRoomDto();
@@ -23,14 +25,15 @@ public class ChatRoomDto {
         chatDto.isPublic = chatRoom.getIsPublic();
         chatDto.memberIds = chatRoom.getMemberIds();
         chatDto.isMember = chatDto.memberIds.contains(userId);
+        chatDto.isPasswordProtected = chatRoom.getPasswordHash().isPresent();
         chatDto.memberCount = chatDto.memberIds.size();
-        
+
         return chatDto;
     }
 
     public static List<ChatRoomDto> fromList(List<ChatRoom> chatRooms, UUID userId) {
-        List<ChatRoomDto> chatsDto = new ArrayList<ChatRoomDto>();
-        for(ChatRoom chat : chatRooms) {
+        List<ChatRoomDto> chatsDto = new ArrayList<>();
+        for (ChatRoom chat : chatRooms) {
             chatsDto.add(from(chat, userId));
         }
         return chatsDto;
@@ -90,6 +93,14 @@ public class ChatRoomDto {
 
     public void setMemberCount(int memberCount) {
         this.memberCount = memberCount;
+    }
+
+    public boolean getIsPasswordProtected() {
+        return this.isPasswordProtected;
+    }
+
+    public void setIsPasswordProtected(boolean isPasswordProtected) {
+        this.isPasswordProtected = isPasswordProtected;
     }
 
 }
